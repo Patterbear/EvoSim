59d51f8da3bb271fa11862d5d726b5fd86bbf5b6
Benjamin McGregor <bwtfm1@student.le.ac.uk>
2022-10-24 13:23:15 +0100

    integrate turtle into tkinter for prototype 1

--- prototypes/organism_gen.py
+++ prototypes/organism_gen.py
@@ -1,7 +1,15 @@
-import turtle
+from random import randint
+from tkinter import *
+from turtle import RawTurtle, TurtleScreen
+
+# Primary Colours Array for testing
+prim_colours = ["red", "blue", "yellow"]
 
 
+# Function to draw circles of a random colour on the canvas
 def draw_circle(x, y):
+    turtle.color(prim_colours[randint(0, 2)])
+
     turtle.penup()
     turtle.setx(x)
     turtle.sety(y)
@@ -12,24 +20,29 @@ def draw_circle(x, y):
     turtle.end_fill()
 
 
-window = turtle.Screen()
+# Root Tkinter window
+root = Tk()
+root.winfo_toplevel().iconphoto(True, Image("photo", file="../EvoSim/assets/icon.png"))
+root.title("EvoSim - Organism Generation")
+root.winfo_toplevel().geometry("600x450")
+
+# Canvas to host the Turtle canvas
+canvas = Canvas(root, width=400, height=400)
+canvas.pack()
+screen = TurtleScreen(canvas)
+turtle = RawTurtle(canvas)
+
+# Turtle configurations
 turtle.speed(0)
 turtle.pensize(5)
 turtle.hideturtle()
 
-# Ensures instant drawing
-turtle.tracer(0)
+# Assigning canvas dimensions to variables and creating limits
+x = canvas.winfo_width() - 200
+y = canvas.winfo_height() - 200
 
-# Circle Outline
-turtle.color("black")
-turtle.fillcolor("black")
+# Button to generate circles at random positions
+Button(root, text="Test", command=lambda: draw_circle(randint(x*-1, x), randint(y*-1, y))).pack()
 
-
-draw_circle(0, 0)
-# draw_circle(turtle.window_width(), turtle.window_height())
-print(turtle.window_width())
-draw_circle(930, 0)
-
-# Ensures instant drawing
-turtle.update()
-turtle.mainloop()
+# Keeps the screen visible
+screen.mainloop()
